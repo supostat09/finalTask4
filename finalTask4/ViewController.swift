ae9f2f7fb30dc8948dced042652be1cd3b0c082f
dzhumadil <dzhumadil.a@icloud.com>
2023-05-02 18:17:57 +0600

    Initial Commit

---
+++ finalTask4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  finalTask4
+//
+//  Created by Абдулла-Бек on 2/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
